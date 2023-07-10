56ee8cd9d9ff2a487bbdd38ea6f68eab2c83e0df
Okan <huseyinokand@gmail.com>
2023-07-10 23:12:38 +0300

    Initial Commit

--- App.tsx
+++ App.tsx
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
+
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>My Name is Trent, and I just created my first expo app!</Text>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +15,11 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+
     backgroundColor: '#fff',
+
     alignItems: 'center',
+
     justifyContent: 'center',
   },
 });
